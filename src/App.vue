078f1dabda3f38f0e99f0425ffd2a49ec6d5b55d
Demon <dmitriy.movch@gmail.com>
2024-12-18 22:44:13 +0200

    save

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-<script>
-
-</script>
-
 <template>
   <main>
-
+		<start-screen></start-screen>
   </main>
 </template>
+
+<script>
+import StartScreen from './components/StartScreen.vue'
+
+export default {
+  components: {
+    StartScreen
+  },
+}
+</script>
 
 <style scoped>
 
---
+++ src/components/StartScreen.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <main>
+		<start-screen-image></start-screen-image>	
+  </main>
+</template>
+
+<script>
+import StartScreenImage from '../images/StartScreenImage.vue'
+
+export default {
+  components: {
+    StartScreenImage
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
